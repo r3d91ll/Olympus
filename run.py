a175046eccdd7cf308c0a024b564416fd3b99a31
r3d91ll <r3d91ll@bucy-medrano.me>
2025-01-03 01:02:28 -0600

    reorganized some file

--- run.py
+++ run.py
@@ -1,5 +1,11 @@
-import subprocess
+import os
+import sys
+
+# Set the PYTHONPATH to include the 'src' directory
+sys.path.append(os.path.join(os.getcwd(), 'src'))
+
+# Import the Reflex app from the OracleDelphi module
+from src.OracleDelphi.app import app
 
 if __name__ == "__main__":
-    # Run Reflex application with custom ports
-    subprocess.run(["reflex", "run"])
+    app.run()
